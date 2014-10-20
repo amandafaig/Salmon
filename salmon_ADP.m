3a37f10017e879a277a000c6adbc0520212faae1
amandafaig <afaig@ucdavis.edu>
2014-10-20 16:32:29 -0700

    ADP working.  Most basic version ever

--- salmon_ADP.m
+++ salmon_ADP.m
@@ -1,7 +1,7 @@
-% First step towards programming economics of salmon hatchery/trucking
-% problem: programming VFI problem with a single state variable (population
+% Second step towards programming economics of salmon hatchery/trucking
+% problem: programming ADP problem with a single state variable (population
 % size and a single choice variable (hatchery output), given a harvest
-% rule.
+% rule, and stocasticity.
 %
 % Eventually we need to have population size, and some phenotypic (or 
 % genotypic) variables as the sate variables, and hatchery output, trucking
@@ -10,7 +10,7 @@
 % ------------------------------------------------------------------------
 % There software includes the following files:
 % ------------------------------------------------------------------------
-% 1. "salmon.m"         Computes the VFI solution in the form of investment
+% 1. "salmon_ADP.m"         Computes the ADP solution in the form of investment
 %                       policy function.
 % 2. "negpayoff.m"      Calculates the value function at each given state.
 %                       This file also includes the hatchery transformation
@@ -31,82 +31,83 @@
 % 4. harvest            harvest = S_t - escapement*S_t
 %                       This should also get updated
 % ------------------------------------------------------------------------
-% Created: September 30, 2014
-% Last Updated: October 6, 2014
+% Created: October 15, 2014
+% Last updated: October 20, 2014
 % Amanda Faig
 % ------------------------------------------------------------------------
 
 clear all
+%dbstop in negpayoff
+rng(2061);  % Seed the random number generator
 
-% 1A. Set up the economic parameters
+% 1A. Determine simulation length
+% -------------------------------
+T       = 1000;
+
+% 1B. Set up the economic parameters
 % ----------------------------------
-p       = 5;            % price of harvest
+p       = 10;           % price per kg
 c       = 0.075;        % cost of harvest
 delta   = 1/1.03;       % discount factor
 esc     = 0.4;          % excapement rule: for now just what portion 
                         % of the stock will be harvested
                         
-
-% 1B. Set up the biological parameters
+% 1C. Set up the biological parameters
 % ------------------------------------
-R       = 11;          
-alpha   = .1;          
+R       = 201;          
+alpha   = 2;          
 K       = round((R-1)/alpha);          % carrying capacity
 
-% 1C. Set up stochasticity
+% 1D. Set up stochasticity
 % ------------------------
-z       = [0.3; 0; -0.3];       % the shocks to salmon growth
-pz      = [0.3, 0.4, 0.3];      % the probability that each shock will 
-                                % occur
-                                
-        % check that the probability vector makes sense
-        % ---------------------------------------------
-        if sum(pz) ~= 1                 
+mean        = 0;                    % mean for distribution of shocks
+sd          = .1;                    % SD for distribution of shocks 
+z           = linspace(mean - 3*sd,mean + 3*sd, 100); 
+                                % 100 points from 99.7% of the distribution
+cz          = normcdf(z,mean,sd);
+cz2         = [0, cz];
+cz2(end)    = [];
+pz          = (cz - cz2);
+pz(end)     = 0;
+pz(end)     = 1 - sum(pz);
 
-            disp('ERROR: improper probability matrix')
-            break
-        end
-
-% 1C. Set up all other parameters
+% 1E. Set up all other parameters
 % -------------------------------
-Svec    = linspace(0,K,K);      % vector of the state space
+Svec    = linspace(0,K,K+1);      % vector of the state space
 check   = 2;
 
-
-% 2. Initial condition
-% --------------------
-Vold    = 2700*ones(1, K);  % sets initial guess of value function for 
-                                % each stock leven in each period
-                                               
+% 2. Initial conditions
+% ---------------------
+Vold    = linspace(8000,8300,K+1);  % sets initial guess of value function for 
+                                    % each stock leven in each period
+V               = Vold;                
+j               = round(K/2);
+S               = Svec(j);
+count           = 0;
+invstar         = 500*ones(K+1,1);
 
 % 3. Value Function Iteration
 % ---------------------------
 
 while check > .01           % keep going until max dev less than .01%
-    for i = 1:length(Svec)  % loop over stocks
-        S               = Svec(i);
-        [Invtmp, Vtmp]  = ...
+   for i = 1:T
+    [Invtmp, Vtmp]  = ...
             fminbnd(@(inv)negpayoff(inv,esc,p,c,delta,R,alpha,Svec,S,Vold,z,pz),0,1000);
-                            % temporary investment decision and value 
-                            % function is equal to the levels that maximize 
-                            % the payoff function (minimize -1*payoff)
-        invstar(i)      = Invtmp;   
-                            % investment decisions for each level of stock
-        V(i)            = -Vtmp;   
-                            % make current value function the starting 
-                            % point for the next iteration
-    end
-    
+    V(j)            = -Vtmp;
+    invstar(j)      = Invtmp;
+    j               = max(round(rand()*(K+1)),1);
+    S               = Svec(j);  
+  end
     dev         = abs((V - Vold)./V)*100;   % calculate the maximum 
                                             % deviation (in percent) 
                                             % between iterations
     check       = max(dev);                 
     Vold        = V;
-    
+     
     % Plot the value function and policy function of each iteration
     % -------------------------------------------------------------
     
-    colorvec    = [0.1 ,0.5 ,1 ; 1, 0, 0];  % creates colormap for plot
+    colorvec    = [0.2 ,1 ,1 ; 1, 0, 0.5];  % creates colormap for plot
     
     if check > 0.01                         % so long as the loop will do 
                                             % another iteration, plot using
